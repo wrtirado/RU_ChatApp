0df2248f754f378339052e50096aba475dac6bca
swimbo <bo.bergstrom@gmail.com>
2016-03-09 15:17:17 -0700

    controller 90% complete. needs testing

--- chatBoxController.js
+++ chatBoxController.js
@@ -4,6 +4,23 @@
     .controller('chatController',chatController)
 
     function chatController(){
-      var chatCTRL = this
+      var chatCTRL = this
+      chatCTRL.chatArray = []
+
+      chatCTRL.inputText = 'Begin typing here'
+      chatCTRL.userName = 'Anonynomous1'
+
+      chatCTRL.submitMessage = function(){
+        var msg = {
+          text : chatCTRL.inputText,
+          username : chatCTRL.userName,
+            }
+            chatCTRL.chatArray.push(msg)
+      }
     }
+
+      // function Anonynomous1(){
+      //should this be inside chat controoler or separate?
+      // }
+
 })();
