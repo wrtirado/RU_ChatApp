02c14b5ca7b5a36cba193dee99f5c633750e9a3f
Christian M. Macy <christian.m.macy@gmail.com>
2016-03-16 11:24:13 -0600

    add ui-view functionality to support routing

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 ;(function () {
   'use strict'
-  angular.module('chatApp', ['chatBoxControllers'])
+  angular.module('chatApp', ['chatBoxControllers', 'ui.router'])
+    .config(MainRouter)
 
+  // MainRouter.$.inject = ['$stateProvider', '$urlRouteProvider']
 
+  function MainRouter ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('chat', {
+        url: "/",
+        templateUrl: "./chatRoom.html",
+      })
+
+      .state('user', {
+        url: "user",
+        templateUrl: "./user.html",
+      })
+
+      .state('signUp',{
+        url: '/signUp',
+        templateUrl: './signup.html'
+      })
+  }
 }())
