5e510b8f880fd4c852cb2c0cc3d61fe04ee4a574
toneloke <david.guerrero87@live.com>
2016-03-16 11:16:49 -0600

    ui-router

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 ;(function () {
   'use strict'
-  angular.module('chatApp', ['chatBoxControllers'])
+  angular.module('chatApp', ['chatBoxControllers', 'ui.router'])
+    .config(myRoutes)
 
+  function myRoutes ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('chat', {url: '/',templateUrl: './chatRoom.html'})
+      .state('user', {url: '/user',templateUrl: './user.html'})
+      .state('signUp',{url: '/signUp',templateUrl: './signup.html'})
+    $urlRouterProvider.otherwise('/')
+  }
 
 }())
